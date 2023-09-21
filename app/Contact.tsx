a50390a229bafb93d12d137597f3ee791583113c
Chandler Sommerville <cmsommerville@gmail.com>
2023-09-21 09:55:36 -0400

    added honeypot to form

--- app/Contact.tsx
+++ app/Contact.tsx
@@ -1,9 +1,44 @@
 "use client";
+import { useState } from "react";
+
+interface FormDataInterface {
+  first_name: string;
+  last_name: string;
+  email: string;
+  about: string;
+  zip_code?: string | undefined | null;
+}
+
+const DEFAULT_FORM_DATA: FormDataInterface = {
+  first_name: "",
+  last_name: "",
+  email: "",
+  about: "",
+};
+
+const validator = (form_data: FormDataInterface) => {
+  // zip_code is a honeypot
+  if (form_data.zip_code) return;
+
+  if (!form_data.first_name) return;
+  if (!form_data.last_name) return;
+  if (!form_data.email) return;
+  if (!form_data.about) return;
+  return form_data;
+};
 
 export default function Contact() {
-  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(e);
+  const [formData, setFormData] =
+    useState<FormDataInterface>(DEFAULT_FORM_DATA);
+
+  const formDataSetter = (key: keyof FormDataInterface, val: string) => {
+    setFormData((prev) => ({ ...prev, [key]: val }));
+  };
+
+  const handleFormSubmit = () => {
+    const data = validator(formData);
+    if (!data) return;
+    console.log(data);
   };
 
   return (
@@ -17,10 +52,7 @@ export default function Contact() {
           clipPath: "polygon(0 0, 100% 20%, 100% 100%, 0 100%)",
         }}
       ></div>
-      <form
-        className="bg-gradient-to-br from-slate-50 to-white dark:from-slate-800 dark:to-slate-700 shadow-t dark:shadow-primary-500 py-8 px-6 md:px-16 rounded-lg absolute z-10 -top-8 w-3/4 md:w-2/3 lg:w-1/2"
-        onSubmit={handleFormSubmit}
-      >
+      <form className="bg-gradient-to-br from-slate-50 to-white dark:from-slate-800 dark:to-slate-700 shadow-t dark:shadow-primary-500 py-8 px-6 md:px-16 rounded-lg absolute z-10 -top-8 w-3/4 md:w-2/3 lg:w-1/2">
         <div className="space-y-12">
           <div className="border-b border-slate-900/10 pb-12">
             <h2 className="text-3xl font-bold tracking-tight leading-7 text-slate-700 dark:text-slate-300">
@@ -44,6 +76,10 @@ export default function Contact() {
                       autoComplete="given-name"
                       className="block flex-1 border border-slate-200 outline-none rounded-md py-1.5 pl-3 text-slate-800 placeholder:text-neutral-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 focus:border-transparent sm:text-sm sm:leading-6  dark:bg-transparent dark:placeholder:text-slate-400 dark:border-slate-500"
                       placeholder="Sally"
+                      onChange={(e) =>
+                        formDataSetter("first_name", e.target.value)
+                      }
+                      value={formData.first_name}
                     />
                   </div>
                 </div>
@@ -64,6 +100,10 @@ export default function Contact() {
                       autoComplete="family-name"
                       className="block flex-1 border border-slate-200 outline-none rounded-md py-1.5 pl-3 text-slate-800 placeholder:text-neutral-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 focus:border-transparent sm:text-sm sm:leading-6  dark:bg-transparent dark:placeholder:text-slate-400 dark:border-slate-500"
                       placeholder="Dunbar"
+                      onChange={(e) =>
+                        formDataSetter("last_name", e.target.value)
+                      }
+                      value={formData.last_name}
                     />
                   </div>
                 </div>
@@ -85,6 +125,8 @@ export default function Contact() {
                       autoComplete="email"
                       className="block flex-1 border border-slate-200 outline-none rounded-md py-1.5 pl-3 text-slate-800 placeholder:text-neutral-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 focus:border-transparent sm:text-sm sm:leading-6  dark:bg-transparent dark:placeholder:text-slate-400 dark:border-slate-500"
                       placeholder="sally@actuarydb.com"
+                      onChange={(e) => formDataSetter("email", e.target.value)}
+                      value={formData.email}
                     />
                   </div>
                 </div>
@@ -105,8 +147,28 @@ export default function Contact() {
                     className="block w-full rounded-md border-0 py-1.5 text-slate-800 shadow-sm ring-1 ring-inset ring-slate-300 placeholder:text-neutral-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6 dark:bg-transparent dark:placeholder:text-slate-400 dark:ring-slate-500"
                     defaultValue={""}
                     placeholder="Tell us a little bit about your project."
+                    onChange={(e) => formDataSetter("about", e.target.value)}
+                    value={formData.about}
                   />
                 </div>
+              </div>
+              <div className="nosend">
+                <label
+                  htmlFor="postal-code"
+                  className="block text-sm font-medium leading-6 text-slate-800 dark:text-slate-400"
+                >
+                  Zip Code*
+                </label>
+                <input
+                  autoComplete="nope"
+                  type="text"
+                  id="postal-code"
+                  name="postal-code"
+                  placeholder="e.g. 90210"
+                  className="block flex-1 border border-slate-200 outline-none rounded-md py-1.5 pl-3 text-slate-800 placeholder:text-neutral-400 focus:ring-2 focus:ring-inset focus:ring-violet-600 focus:border-transparent sm:text-sm sm:leading-6  dark:bg-transparent dark:placeholder:text-slate-400 dark:border-slate-500"
+                  onChange={(e) => formDataSetter("zip_code", e.target.value)}
+                  value={formData.zip_code ?? ""}
+                ></input>
               </div>
             </div>
           </div>
@@ -114,8 +176,9 @@ export default function Contact() {
 
         <div className="mt-6 flex items-center justify-end gap-x-6">
           <button
-            type="submit"
+            type="button"
             className="mb-2 inline-block rounded bg-primary-500 px-10 py-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] md:mr-2 md:mb-0"
+            onClick={handleFormSubmit}
           >
             Send
           </button>
